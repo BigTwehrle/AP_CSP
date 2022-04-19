a7dad55e77ed2bd86eef8c1637018f2ab6341f21
BigTwehrle <67010826+BigTwehrle@users.noreply.github.com>
2022-04-19 12:12:48 -0400

    Added wrapper class to input to provide more metadata.

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,58 @@
 #include <chrono>
 #include "algo/algo.hpp"
 
-int list[1000];
+struct Option{
+    std::string opt_name;
+    int opt_num;
 
-void compare(const std::vector<int>& options){
+    Option(int num){
+        switch (num){
+            case 0:
+                this->opt_name = "compare";
+                this->opt_num = num;
+            case 1:
+                this->opt_name = "selection sort";
+                this->opt_num = num;
+                break;
+            case 2:
+                this->opt_name = "buble sort";
+                this->opt_num = num;
+                break;
+            default:
+                this->opt_name = "invalid";
+                this->opt_num = num;
+        }
+    }
+};
+
+template <size_t S>
+void list_init(int low, int high, int (&list)[S]){
+    for (auto& x : list)
+        x = (rand() % (high - low)) + low;
+}
+
+template <size_t S>
+void print_list(int (&list)[S]){
+    std::cout << "List contents: ";
+
+    for (auto& x : list)
+        std::cout << x << ' ';
+
+    std::endl(std::cout);
+}
+
+void compare(const std::vector<Option>& options){
     int list[1000];
 
     std::cout << "Please input a range of data to sort between <low:high> -2147483648 and 2147483647:\n";
     int low, high;
     std::cin >> low >> high;
 
-    list_init(low, high);
+    list_init(low, high, list);
+
+    print_list<1000>(list);
+
+    algo::selection_sort(list, 1000);
 
     print_list<1000>(list);
 }
@@ -33,8 +75,8 @@ void receive_input(int& input){
     }
 }
 
-void get_options(std::vector<int>& options){
-    using const_it = std::vector<int>::const_iterator;
+void get_options(std::vector<Option>& options){
+    using const_it = std::vector<Option>::const_iterator;
     int input = -1;
 
     std::cout << "\nPlease add a sorting algorithm (1-2), press (0) to compare, or anything else to exit:\n";
@@ -46,14 +88,15 @@ void get_options(std::vector<int>& options){
     do{
         bool removed = false;
         receive_input(input);
+        Option option(input); 
 
-        if (input > 0 && input <= 2){
+        if (option.opt_num > 0 && option.opt_num <= 2){
             for (const_it begin = options.begin(),
                 end = options.end();
                 begin != end;
                 ++begin){
-                if (*begin == input){
-                    std::cout << "[ Removed option " << *begin << " from comparison ]\n";
+                if (begin->opt_num == option.opt_num){
+                    std::cout << "[ Removed option name: " << option.opt_name << ", id: " << option.opt_num << " from comparison ]\n";
                     options.erase(begin);
                     removed = true;
                 } 
@@ -61,11 +104,11 @@ void get_options(std::vector<int>& options){
 
             if (!removed)
             {
-                std::cout << "[ Added option " << input << " to comparison ]\n";
-                options.push_back(input);
+                std::cout << "[ Added option name: " << option.opt_name << ", id: " << option.opt_num << " to comparison ]\n";
+                options.push_back(option);
             } 
         }
-        else if (input == 0){
+        else if (option.opt_num == 0){
             if (options.size() < 2){
                 std::cout << "Must have at least two options selected to compare!\n";
                 input = 1;
@@ -88,25 +131,10 @@ void get_options(std::vector<int>& options){
     }
 }
 
-void list_init(int low, int high){
-    for (auto& x : list)
-        x = (rand() % (high - low)) + low;
-}
-
-template <size_t S>
-void print_list(int (&list)[S]){
-    std::cout << "List contents: ";
-
-    for (auto& x : list)
-        std::cout << x << ' ';
-
-    std::endl(std::cout);
-}
-
 int main(){
     srand(time(NULL));
 
-    std::vector<int> options;
+    std::vector<Option> options;
     
     get_options(options);
 
