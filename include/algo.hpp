b87c1cdb1da2e40098780f5aa2703b83a8eb15c4
BigTwehrle <67010826+BigTwehrle@users.noreply.github.com>
2022-04-21 11:24:56 -0400

    Forgot to commit changes in headerfiles for quick sort implementation.

--- include/algo.hpp
+++ include/algo.hpp
@@ -10,16 +10,53 @@ namespace algo{
             x = y;
             y = temp;
         }
+
+        int partition(int arr[], int start, int end, std::pair<int, int>& results)
+        {
+        
+            int pivot = arr[start];
+        
+            int count{};
+            for (int i = start + 1; i <= end; i++) {
+                if (arr[i] <= pivot)
+                    ++count, ++results.first;
+            }
+        
+            int pivot_index = start + count;
+            swap(arr[pivot_index], arr[start]);
+            ++results.second;
+        
+            int i = start, j = end;
+        
+            while (i < pivot_index && j > pivot_index) {
+        
+                while (arr[i] <= pivot) {
+                    i++;
+                }
+        
+                while (arr[j] > pivot) {
+                    j--;
+                }
+        
+                if (i < pivot_index && j > pivot_index) {
+                    swap(arr[i++], arr[j--]);
+                    ++results.second;
+                }
+
+                ++results.first;
+            }
+        
+            return pivot_index;
+        }
     }
 
-    template<typename func>
-    std::pair<int, int> selection_sort(int arr[], int n, func compare);
+    void selection_sort(int arr[], int n, std::pair<int, int>& results);
 
-    template<typename func>
-    std::pair<int, int> bubble_sort(int arr[], int n, func compare);
+    void bubble_sort(int arr[], int n, std::pair<int, int>& results);
 
-    template<typename func>
-    std::pair<int, int> insertion_sort(int arr[], int n, func compare);
+    void insertion_sort(int arr[], int n, std::pair<int, int>& results);
+
+    void quick_sort(int arr[], int begin, int end, std::pair<int, int>& results);
 }
 
 #endif
